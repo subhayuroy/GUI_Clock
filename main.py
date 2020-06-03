2b6606f6a251828e4011488ff2633f96f7a91434
subhayuroy <subhayu.roy2001@gmail.com>
2020-06-03 21:30:26 +0530

    Fixing X-axis and Y-axis

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ try:
 except ImportError:
     hasPIL = False
 
+class mapper:
+    def __init__(self, world, viewpoint):
+        self.world = world
+        self.viewpoint = viewpoint
+        x_min, y_min, x_max, y_max = self.world
+        X_min, Y_min, X_max, Y_max = self.viewpoint
+        f_x = float(X_max - X_min) / float(x_max - x_min)
+        f_y = float(Y_max - Y_min) / float(y_max - y_min)
+        self.f = min(f_x, f_y)
+        x_c = 0.5 * (x_min + x_max)
+        y_c = 0.5 * (y_min + y_max)
+        X_c = 0.5 * (X_min + X_max)
+        Y_c = 0.5 * (Y_min + Y_max)
+        self.c_1 = X_c - self.f * x_c
+        self.c_2 = Y_c - self.f * y_c
+    def __windowToViewport(self, x, y):
+        X = self.f * x + self.c_1
+        Y = self.f * -y + self.c_2
+        return X, Y
+    def windowToViewport(self, x1, y1, x2, y2):
+        return self.__windowToViewport(x1, y1), self.__windowToViewport(x2, y2)
+
